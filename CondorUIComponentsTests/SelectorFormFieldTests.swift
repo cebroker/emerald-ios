2380f75a9851e3178aab625e66d1d49c1529277e
Luis David Goyes Garces <lgoyes@condorlabs.io>
2019-03-06 17:05:58 -0500

    tests fixed

--- CondorUIComponentsTests/SelectorFormFieldTests.swift
+++ CondorUIComponentsTests/SelectorFormFieldTests.swift
@@ -11,40 +11,45 @@ import XCTest
 @testable import CondorUIComponents
 
 class SelectorFormFieldTests: XCTestCase {
-    
+
     private struct InnerConstants {
         struct Strings {
             static let empty = ""
         }
-        
+
         struct Settings {
             static let mockFrame = Constants.MockedData.mockFrame
         }
-        
+
         struct Values {
             static let successfulState = Constants.MockedData.states[0]
         }
     }
-    
-    var stateSelectorField: SelectorFormField?
-    
+
+    var stateSelectorField: FormFieldType<String>?
+
     override func setUp() {
         stateSelectorField = SelectorFormField(frame: Constants.MockedData.mockFrame)
     }
-    
+
     override func tearDown() {
         stateSelectorField = nil
     }
-    
+
     func testShouldPassValidation() {
-        guard let stateSelectorField = self.stateSelectorField as? SelectorFormFieldProtocol else {
+        guard let stateSelectorField = self.stateSelectorField else {
             XCTFail(Constants.TestsErrors.formFieldCastingError.localizedDescription)
             return
         }
         
-        stateSelectorField.set(data: Constants.MockedData.states)
-        stateSelectorField.set(selectedRow: InnerConstants.Values.successfulState)
-        
+        guard let stateSelectorFieldDataSetter = self.stateSelectorField as? SelectorFormFieldProtocol else {
+            XCTFail(Constants.TestsErrors.formFieldCastingError.localizedDescription)
+            return
+        }
+
+        stateSelectorFieldDataSetter.set(data: Constants.MockedData.states)
+        stateSelectorFieldDataSetter.set(selectedRow: InnerConstants.Values.successfulState)
+
         guard stateSelectorField.isValid().isValid else {
             if let error = stateSelectorField.isValid().error?.description {
                 XCTFail(error)
@@ -54,88 +59,97 @@ class SelectorFormFieldTests: XCTestCase {
             return
         }
     }
-    
+
     func testShouldNotPassValidationEmpty() {
-        guard let stateSelectorField = self.stateSelectorField as? SelectorFormFieldProtocol else {
+        guard let stateSelectorField = self.stateSelectorField else {
             XCTFail(Constants.TestsErrors.formFieldCastingError.localizedDescription)
             return
         }
         
-        stateSelectorField.set(data: Constants.MockedData.states)
-        
+        guard let stateSelectorFieldDataSetter = self.stateSelectorField as? SelectorFormFieldProtocol else {
+            XCTFail(Constants.TestsErrors.formFieldCastingError.localizedDescription)
+            return
+        }
+
+        stateSelectorFieldDataSetter.set(data: Constants.MockedData.states)
+
         let validationResult = stateSelectorField.isValid()
-        
+
         guard !validationResult.isValid else {
             XCTFail(Constants.TestsErrors.validationSucceded.localizedDescription)
             return
         }
-        
+
         guard let errorDescription = validationResult.error?.description else {
             XCTFail(Constants.TestsErrors.unexpectedError.localizedDescription)
             return
         }
-        
+
         guard errorDescription == FormFieldError.emptyField.description else {
             XCTFail(errorDescription)
             return
         }
     }
-    
+
     func testShouldNotPassValidationMissingSelectedRow() {
-        guard let stateSelectorField = self.stateSelectorField as? SelectorFormFieldProtocol else {
+        guard let stateSelectorField = self.stateSelectorField else {
             XCTFail(Constants.TestsErrors.formFieldCastingError.localizedDescription)
             return
         }
         
-        stateSelectorField.set(data: [Constants.MockedData.states[0]])
-        stateSelectorField.set(selectedRow: Constants.MockedData.states[1])
-        
+        guard let stateSelectorFieldDataSetter = self.stateSelectorField as? SelectorFormFieldProtocol else {
+            XCTFail(Constants.TestsErrors.formFieldCastingError.localizedDescription)
+            return
+        }
+
+        stateSelectorFieldDataSetter.set(data: [Constants.MockedData.states[0]])
+        stateSelectorFieldDataSetter.set(selectedRow: Constants.MockedData.states[1])
+
         let validationResult = stateSelectorField.isValid()
-        
+
         guard !validationResult.isValid else {
             XCTFail(Constants.TestsErrors.validationSucceded.localizedDescription)
             return
         }
-        
+
         guard let errorDescription = validationResult.error?.description else {
             XCTFail(Constants.TestsErrors.unexpectedError.localizedDescription)
             return
         }
-        
+
         guard errorDescription == SelectorFormFieldError.missingSelectedValue.description else {
             XCTFail(errorDescription)
             return
         }
     }
-    
+
     func testShouldNotPassValidationUISelectedValueMismatch() {
-        guard let stateSelectorField = self.stateSelectorField as? SelectorFormFieldProtocol else {
+        guard let stateSelectorField = self.stateSelectorField else {
             XCTFail(Constants.TestsErrors.formFieldCastingError.localizedDescription)
             return
         }
         
-        stateSelectorField.set(data: [Constants.MockedData.states[0]])
-        stateSelectorField.set(selectedRow: Constants.MockedData.states[0])
-        
-        guard let testableStateSelectorField = self.stateSelectorField as? TestableTextFormFieldProtocol else {
+        guard let stateSelectorFieldDataSetter = self.stateSelectorField as? SelectorFormFieldProtocol else {
             XCTFail(Constants.TestsErrors.formFieldCastingError.localizedDescription)
             return
         }
-        
-        testableStateSelectorField.set(text: Constants.MockedData.states[0].name + " ")
-        
+
+        stateSelectorFieldDataSetter.set(data: [Constants.MockedData.states[0]])
+        stateSelectorFieldDataSetter.set(selectedRow: Constants.MockedData.states[0])
+        stateSelectorFieldDataSetter.set(text: Constants.MockedData.states[0].name + " ")
+
         let validationResult = stateSelectorField.isValid()
-        
+
         guard !validationResult.isValid else {
             XCTFail(Constants.TestsErrors.validationSucceded.localizedDescription)
             return
         }
-        
+
         guard let errorDescription = validationResult.error?.description else {
             XCTFail(Constants.TestsErrors.unexpectedError.localizedDescription)
             return
         }
-        
+
         guard errorDescription == SelectorFormFieldError.uiSelectedValueMismatch.description else {
             XCTFail(errorDescription)
             return
