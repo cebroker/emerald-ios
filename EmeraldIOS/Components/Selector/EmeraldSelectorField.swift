306bebc44b9ec97419aaa1b9aab3ae2a71b4f7db
Genesis Sanguino <karazgs@gmail.com>
2019-05-09 11:14:06 -0500

    Working on Selector component

---
+++ EmeraldIOS/Components/Selector/EmeraldSelectorField.swift
@@ -0,0 +1,9 @@
+//
+//  EmeraldSelectorField.swift
+//  EmeraldIOS
+//
+//  Created by Genesis Sanguino on 5/9/19.
+//  Copyright © 2019 Condor Labs. All rights reserved.
+//
+
+import Foundation
---
+++ EmeraldIOS/Components/Selector/EmeraldSelectorFieldStyle.swift
@@ -0,0 +1,9 @@
+//
+//  EmeraldSelectorFieldStyle.swift
+//  EmeraldIOS
+//
+//  Created by Genesis Sanguino on 5/9/19.
+//  Copyright © 2019 Condor Labs. All rights reserved.
+//
+
+import Foundation
