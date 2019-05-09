3653f3e118a50a27896010e9c65e1521f29b1581
Genesis Sanguino <karazgs@gmail.com>
2019-05-09 15:12:37 -0500

    Working on EmeraldSelectorField

--- EmeraldIOS/Components/Selector/EmeraldSelectorField.swift
+++ EmeraldIOS/Components/Selector/EmeraldSelectorField.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2019 Condor Labs. All rights reserved.
 //
 
-import Foundation
+import UIKit
+//EmeraldTextFieldStyle
+public protocol EmeraldSelectorFieldType: EmeraldTextFormFieldType {
+    func set(data: [Selectable])
+    func clearData()
+    func getSelectedRow() -> Selectable?
+    func set(selectedRow: Selectable)
+    func set(notifiable: EmeraldSelectorFieldChangeNotifiable?)
+}
+
+public class EmeraldSelectorField: EmeraldTextField, EmeraldSelectorFieldType, UIPickerViewDelegate, UIPickerViewDataSource {
+    public func set(notifiable: EmeraldSelectorFieldChangeNotifiable?) {
+        <#code#>
+    }
+    
+    public func set(data: [Selectable]) {
+        <#code#>
+    }
+    
+    public func clearData() {
+        <#code#>
+    }
+    
+    public func getSelectedRow() -> Selectable? {
+        <#code#>
+    }
+    
+    public func set(selectedRow: Selectable) {
+        <#code#>
+    }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+}
--- EmeraldIOS/Components/Selector/EmeraldSelectorFieldStyle.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EmeraldSelectorFieldStyle.swift
-//  EmeraldIOS
-//
-//  Created by Genesis Sanguino on 5/9/19.
-//  Copyright © 2019 Condor Labs. All rights reserved.
-//
-
-import Foundation
