0288582d3bb2b61fe6a1a852e259fcb5b85571dd
Genesis Sanguino <karazgs@gmail.com>
2019-05-17 10:46:09 -0500

    Icon changed

--- EmeraldIOS/Components/Signature/SignatureCreator/EmeraldSignatureView.swift
+++ EmeraldIOS/Components/Signature/SignatureCreator/EmeraldSignatureView.swift
@@ -212,8 +212,8 @@ public class EmeraldSignatureView: UIView {
     }
     
     private func setupImages() {
-        self.xIcon = UIImage(named: "xIcon.png",
-                             in: Bundle(for: EmeraldSignatureView.self),
+        self.xIcon = UIImage(named: "xIconAsset",
+                             in: Bundle(identifier: "io.condorlabs.EmeraldIOS"),
                              compatibleWith: nil)?
             .withRenderingMode(.alwaysTemplate)
         
