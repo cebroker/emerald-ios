870aaf1fdd9e997d4dc6d766c11faafe0f483458
Carlos Pava <cpava@condorlabs.io>
2019-11-19 16:00:11 -0500

    set status to emeraldregextextfield.

--- EmeraldIOS/Components/TextField/RegexField/EmeraldRegexTextField.swift
+++ EmeraldIOS/Components/TextField/RegexField/EmeraldRegexTextField.swift
@@ -15,7 +15,7 @@ public protocol EmeraldRegexTextFieldType: EmeraldTextFieldType {
 
 @IBDesignable
 public class EmeraldRegexTextField: EmeraldTextField, EmeraldRegexTextFieldType {
-    
+
     @IBInspectable
     public var regex: String {
         set {
@@ -25,29 +25,43 @@ public class EmeraldRegexTextField: EmeraldTextField, EmeraldRegexTextFieldType 
             return self.getRegex().regex!
         }
     }
-    
+
     private var innerRegex: EmeraldRegexFormatType = .any
-    
+
     public func set(regex: EmeraldRegexFormatType) {
         self.innerRegex = regex
     }
-    
+
     public func getRegex() -> EmeraldRegexFormatType {
         return self.innerRegex
     }
-    
+
     override func validateContent() -> Result<Bool, Error> {
         guard let text = self.getValue(), !text.isEmpty,
             let validationRegex = innerRegex.regex else {
-            return .failure(FormFieldError.emptyField)
+                return .failure(FormFieldError.emptyField)
         }
-        
+
         let regex = NSRegularExpression(validationRegex)
-        
+
         guard regex.matchesString(with: text) else {
             return .failure(EmeraldRegexTextFieldError.notMatchingRegex)
         }
-        
+
         return .success(true)
+    }
+
+    public override func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let validationRegex = innerRegex.regex, let text = textField.text else {
+            return false
+        }
+        let textRange = Range(range, in: text)
+        let updatedText = text.replacingCharacters(in: textRange!, with: string)
+        let regex = NSRegularExpression(validationRegex)
+        let value = regex.matchesString(with: updatedText)
+
+        self.setStatus(value ? .valid : .error)
+
+        return super.textField(textField, shouldChangeCharactersIn: range, replacementString: string)
     }
 }
