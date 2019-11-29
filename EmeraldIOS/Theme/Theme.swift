636e27a2b721a575518928185e30733b6c4f4f11
Genesis Sanguino <gsanguino@condorlabs.io>
2019-11-29 09:52:45 -0500

    Fixed segmented bar, languages and colors

--- EmeraldIOS/Theme/Theme.swift
+++ EmeraldIOS/Theme/Theme.swift
@@ -11,25 +11,26 @@ import UIKit
 public class EmeraldTheme {
 
     // MARK: Base Colors
-    public static let whiteColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-    public static let blackColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-    public static let redColor = #colorLiteral(red: 0.8705882353, green: 0.2078431373, blue: 0.0431372549, alpha: 1)
-    public static let greenColor = #colorLiteral(red: 0, green: 0.662745098, blue: 0.3137254902, alpha: 1)
-    public static let blueColor = #colorLiteral(red: 0, green: 0.4509803922, blue: 0.9137254902, alpha: 1)
-    public static let yellowColor = #colorLiteral(red: 1, green: 0.6666666667, blue: 0, alpha: 1)
-    public static let grayColor = #colorLiteral(red: 0.2941176471, green: 0.3254901961, blue: 0.3254901961, alpha: 1)
-    public static let darkGrayColor = #colorLiteral(red: 0.1568627451, green: 0.1803921569, blue: 0.1803921569, alpha: 1)
-    public static let extraDarkGrayColor = #colorLiteral(red: 0.1137254902, green: 0.1294117647, blue: 0.1294117647, alpha: 1)
-    public static let lightGrayColor = #colorLiteral(red: 0.2941176471, green: 0.3254901961, blue: 0.3254901961, alpha: 1)
-    public static let extraLightGrayColor = #colorLiteral(red: 0.3764705882, green: 0.4823529412, blue: 0.4823529412, alpha: 1)
-    public static let toastSuccess = #colorLiteral(red: 0.8862745098, green: 0.9450980392, blue: 0.8784313725, alpha: 1)
-    public static let toastFailure = #colorLiteral(red: 1, green: 0.9181403518, blue: 0.932766974, alpha: 1)
-    public static let axisChart = #colorLiteral(red: 0.9019607843, green: 0.9176470588, blue: 0.9333333333, alpha: 1)
+    private static let bundle = Bundle(for: EmeraldTheme.self)
+    public static let whiteColor = UIColor(named: "whiteColor", in: bundle, compatibleWith: nil)!
+    public static let blackColor = UIColor(named: "blackColor", in: bundle, compatibleWith: nil)!
+    public static let redColor = UIColor(named: "redColor", in: bundle, compatibleWith: nil)!
+    public static let greenColor = UIColor(named: "greenColor", in: bundle, compatibleWith: nil)!
+    public static let blueColor = UIColor(named: "blueColor", in: bundle, compatibleWith: nil)!
+    public static let yellowColor = UIColor(named: "yellowColor", in: bundle, compatibleWith: nil)!
+    public static let grayColor = UIColor(named: "grayColor", in: bundle, compatibleWith: nil)!
+    public static let darkGrayColor = UIColor(named: "darkGrayColor", in: bundle, compatibleWith: nil)!
+    public static let extraDarkGrayColor = UIColor(named: "extraDarkGrayColor", in: bundle, compatibleWith: nil)!
+    public static let lightGrayColor = UIColor(named: "lightGrayColor", in: bundle, compatibleWith: nil)!
+    public static let extraLightGrayColor = UIColor(named: "extraLightGrayColor", in: bundle, compatibleWith: nil)!
+    public static let toastSuccess = UIColor(named: "toastSuccess", in: bundle, compatibleWith: nil)!
+    public static let toastFailure = UIColor(named: "toastFailure", in: bundle, compatibleWith: nil)!
+    public static let axisChart = UIColor(named: "axisChartColor", in: bundle, compatibleWith: nil)!
     
     // MARK: General Colors
     public static let primaryColor = blueColor
-    public static let secondaryColor = #colorLiteral(red: 0.0431372549, green: 0.1647058824, blue: 0.3607843137, alpha: 1)
-    public static let backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9568627451, blue: 0.9725490196, alpha: 1)
+    public static let secondaryColor = UIColor(named: "secondaryColor", in: bundle, compatibleWith: nil)!
+    public static let backgroundColor = UIColor(named: "backgroundColor", in: bundle, compatibleWith: nil)!
     public static let darkTintColor = blackColor.withAlphaComponent(0.15)
     public static let lightTintColor = whiteColor.withAlphaComponent(0.15)
     public static let textColor = darkGrayColor
@@ -44,7 +45,7 @@ public class EmeraldTheme {
     public static let errorButtonHighlightedColor = errorColor.withAlphaComponent(0.15)
     public static let secondaryButtonColor = whiteColor
     public static let placeholderColor = UIColor.lightGray
-    public static let borderColor = #colorLiteral(red: 0.9019607843, green: 0.9098039216, blue: 0.9254901961, alpha: 1)
+    public static let borderColor = UIColor(named: "borderColor", in: bundle, compatibleWith: nil)!
     public static let chevronColor = #colorLiteral(red: 0.7803921569, green: 0.7803921569, blue: 0.8, alpha: 1)
     public static let warningColor = yellowColor
     public static let successColor = greenColor
@@ -53,7 +54,7 @@ public class EmeraldTheme {
 
     // MARK: App Colors
     public static let mainNavigationTintColor = whiteColor
-    public static let mainNavigationBarTintColor = #colorLiteral(red: 0.05490196078, green: 0.2705882353, blue: 0.4862745098, alpha: 1)
+    public static let mainNavigationBarTintColor = UIColor(named: "mainNavigationBarTintColor", in: bundle, compatibleWith: nil)!
     public static let skeletonBackgroundColor = #colorLiteral(red: 0.7450980392, green: 0.7764705882, blue: 0.7764705882, alpha: 1)
     public static let pagerBackgroundColor = #colorLiteral(red: 0.9019607843, green: 0.937254902, blue: 0.9529411765, alpha: 1)
     public static let progressCompletedColor = greenColor
