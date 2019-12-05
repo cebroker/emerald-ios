20f59409c34cb9f42897398f7ea995adefb9783b
Carlos Pava <cpava@condorlabs.io>
2019-12-05 09:44:28 -0500

    added public methods.

--- EmeraldIOS/Components/TextfieldView/EmeraldTextFieldView.swift
+++ EmeraldIOS/Components/TextfieldView/EmeraldTextFieldView.swift
@@ -15,83 +15,83 @@ public class EmeraldTextFieldView: BaseInputView {
         self.setDelegate(self)
     }
 
-    func getPlaceholder() -> String? {
+    public func getPlaceholder() -> String? {
         return self.textField.placeholder
     }
     
-    func set(hint: String?) {
+    public func set(hint: String?) {
         self.textField.set(hint: hint)
     }
     
-    func getHint() -> String? {
+    public func getHint() -> String? {
         return self.textField.getHint()
     }
     
-    func set(maxLength: Int) {
+    public func set(maxLength: Int) {
         self.textField.set(maxLength: maxLength)
     }
     
-    func getMaxLength() -> Int {
+    public func getMaxLength() -> Int {
         return self.textField.getMaxLength()
     }
     
-    func set(format: TextFormat) {
+    public func set(format: TextFormat) {
         self.textField.set(format: format)
     }
     
-    func getFormat() -> TextFormat {
+    public func getFormat() -> TextFormat {
         return self.textField.getFormat()
     }
     
-    func getUnformattedText() -> String? {
+    public func getUnformattedText() -> String? {
         return self.textField.getUnformattedText()
     }
     
-    func set(inputType: UIKeyboardType) {
+    public func set(inputType: UIKeyboardType) {
         self.textField.set(inputType: inputType)
     }
     
-    func getInputType() -> UIKeyboardType? {
+    public func getInputType() -> UIKeyboardType? {
         return self.textField.getInputType()
     }
     
-    func set(id: String?) {
+    public func set(id: String?) {
         self.textField.set(id: id)
     }
     
-    func getId() -> String? {
+    public func getId() -> String? {
         return self.textField.getId()
     }
     
-    func set(isRequired: Bool) {
+    public func set(isRequired: Bool) {
         self.textField.set(isRequired: isRequired)
     }
     
-    func getIsRequired() -> Bool {
+    public func getIsRequired() -> Bool {
         return self.textField.getIsRequired()
     }
     
-    func isValid() -> Result<Bool, Error> {
+    public func isValid() -> Result<Bool, Error> {
         return self.textField.isValid()
     }
     
-    func show(error: FormFieldErrorType){
+    public func show(error: FormFieldErrorType){
         self.textField.show(error: error)
     }
     
-    func clearError() {
+    public func clearError() {
         self.textField.clearError()
     }
     
-    func setText(with value: String?) {
+    public func setText(with value: String?) {
         self.textField.setText(with: value)
     }
     
-    func clearText() {
+    public func clearText() {
         self.textField.clearText()
     }
     
-    func isEnable(_ enable: Bool) {
+    public func isEnable(_ enable: Bool) {
         self.textField.isEnable(enable)
     }
 }
