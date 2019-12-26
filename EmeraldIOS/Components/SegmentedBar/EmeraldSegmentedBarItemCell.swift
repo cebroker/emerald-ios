d2efdadd55ed0346d54b09f0bc463c5860ac25ba
Sergio Giraldo <SGiraldo@condorlabs.io>
2019-12-26 10:51:29 -0500

    Always white color for text in avatar, new emerald chip style, new segmented bar style was added. also was changed some chart color

--- EmeraldIOS/Components/SegmentedBar/EmeraldSegmentedBarItemCell.swift
+++ EmeraldIOS/Components/SegmentedBar/EmeraldSegmentedBarItemCell.swift
@@ -9,23 +9,23 @@
 import UIKit
 
 class EmeraldSegmentedBarItemCell: EmeraldSegmentedBarBaseCell {
-    
+
     static var defaultTheme = SegmentedTitleStyle.primary
-    
+
     override var isHighlighted: Bool {
         didSet {
             titleLabel.textColor = self.desiredColor(isHighlighted)
             titleLabel.font = self.desiredFont(isHighlighted)
         }
     }
-    
+
     override var isSelected: Bool {
         didSet {
             titleLabel.textColor = self.desiredColor(isSelected)
             titleLabel.font = self.desiredFont(isSelected)
         }
     }
-    
+
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = self.desiredColor(false)
@@ -33,7 +33,7 @@ class EmeraldSegmentedBarItemCell: EmeraldSegmentedBarBaseCell {
         label.textAlignment = .center
         return label
     }()
-    
+
     override func setupViews() {
         super.setupViews()
         contentView.addSubview(titleLabel)
@@ -43,16 +43,24 @@ class EmeraldSegmentedBarItemCell: EmeraldSegmentedBarBaseCell {
         titleLabel.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor).isActive = true
         titleLabel.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
     }
-    
+
     func setTitle(_ title: String) {
-        titleLabel.text = title
+        titleLabel.attributedText = self.addStringSpacing(to: title)
     }
-    
+
+    func addStringSpacing(to title: String) -> NSAttributedString {
+        return NSAttributedString(
+            string: title,
+            attributes: [
+                NSAttributedString.Key.kern: EmeraldSegmentedBarItemCell.defaultTheme.titleSpacing
+            ])
+    }
+
     private func desiredColor(_ isSelected: Bool) -> UIColor {
         let theme = EmeraldSegmentedBarItemCell.defaultTheme
         return isSelected ? theme.colorWhenSelected : theme.colorWhenHighlighted
     }
-    
+
     private func desiredFont(_ isSelected: Bool) -> UIFont {
         let theme = EmeraldSegmentedBarItemCell.defaultTheme
         return isSelected ? theme.fontWhenSelected : theme.fontWhenHighlighted
