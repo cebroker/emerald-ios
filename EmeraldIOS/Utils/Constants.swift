059cae8552cce3c3ba8d5272bb74896660750781
Miguel Roncallo <mroncallo@condorlabs.io>
2020-02-11 09:31:32 -0500

    [feat]: Add alternate phone regex

--- EmeraldIOS/Utils/Constants.swift
+++ EmeraldIOS/Utils/Constants.swift
@@ -44,6 +44,7 @@ struct Constants {
         static let ssn = "^[0-9]{9}$"
         static let npi = "^[0-9]{10}$"
         static let phone = "^[0-9]{3}[-]{1}[0-9]{3}[-]{1}[0-9]{4}$"
+        static let alternatePhone = "^[(]{1}[0-9]{3}[)]{1}\\s{1}[0-9]{3}[-]{1}[0-9]{4}$"
         static let zip = "^[0-9]{5}$"
         static let visa = "^[A-Z0-9a-z]*$"
         static let email = "^[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])*$"
