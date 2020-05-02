b10fe14f6f069fc093707628c1bce9ecd5e5e6e3
Sergio Giraldo <SGiraldo@condorlabs.io>
2020-05-02 02:05:21 -0500

    Moving EmeraldTextFieldType method for an extension

--- EmeraldIOS/Components/Text/TextField/EmeraldTextField.swift
+++ EmeraldIOS/Components/Text/TextField/EmeraldTextField.swift
@@ -16,7 +16,7 @@ public enum EmeraldTextFieldState {
 }
 
 @IBDesignable
-public class EmeraldTextField: UITextFieldType, EmeraldTextFieldType, TextFormatter {
+public class EmeraldTextField: UITextFieldType, TextFormatter {
     // MARK: - Constants
     private struct InnerConstants {
         static let middleFontSize: CGFloat = FontSize.h6.cgFontSize
@@ -41,7 +41,6 @@ public class EmeraldTextField: UITextFieldType, EmeraldTextFieldType, TextFormat
 
     // MARK: - Properties
     // MARK: - IBOutlet
-
     @IBOutlet open weak var nextResponderField: UIResponder? {
         didSet {
             if nextResponderField != nil {
@@ -234,158 +233,11 @@ public class EmeraldTextField: UITextFieldType, EmeraldTextFieldType, TextFormat
         customTextFieldDelegate?.didEndEditing?(textField: textField)
     }
 
-    public func clearText() {
-        setText(with: nil)
-    }
-
-    public func set(inputType: UIKeyboardType) {
-        keyboardType = inputType
-    }
-
-    public func getInputType() -> UIKeyboardType? {
-        return keyboardType
-    }
-
-    public func set(id: String?) {
-        self.id = id
-    }
-
-    public func getId() -> String? {
-        return id
-    }
-
-    public func set(placeholder: String?) {
-        initialPlaceHolder = placeholder
-        placeholderLabel.text = placeholder
-        placeholderLabel.textColor = EmeraldTheme.placeholderColor
-    }
-
-    public func getPlaceholder() -> String? {
-        return initialPlaceHolder
-    }
-
-    public func set(hint: String?) {
-        self.hint = hint
-    }
-
-    public func getHint() -> String? {
-        return hint
-    }
-
-    public func set(isRequired: Bool) {
-        self.isRequired = isRequired
-    }
-
-    public func getIsRequired() -> Bool {
-        return isRequired
-    }
-
-    public func set(maxLength: Int) {
-        if maxLength > 0 {
-            self.maxLength = maxLength
-        }
-    }
-
-    public func isEnable(_ enable: Bool) {
-        alpha = enable == true ? 1 : 0.5
-        isEnabled = enable
-        deactivateField()
-    }
-
-    public func getMaxLength() -> Int {
-        return maxLength
-    }
-
-    public func set(format: TextFormat) {
-        innerFormat = format
-        switch format {
-        case .currency:
-            set(maxLength: InnerConstants.maximumDoubleLength)
-            set(inputType: .decimalPad)
-        case .number:
-            set(inputType: .numberPad)
-        case .longDate:
-            set(maxLength: InnerConstants.maximumDateLength)
-            set(inputType: .numberPad)
-        case .shortDate:
-            set(maxLength: InnerConstants.maximumShortDateLength)
-            set(inputType: .numberPad)
-        default:
-            set(inputType: .asciiCapable)
-        }
-    }
-
-    public func getFormat() -> TextFormat {
-        return innerFormat
-    }
-
-    public func getUnformattedText() -> String? {
-        guard let textWithFormat = text else {
-            return nil
-        }
-
-        do {
-            return try remove(format: innerFormat, to: textWithFormat)
-        } catch {
-            return nil
-        }
-    }
-
-    public func isValid() -> Result<Bool, Error> {
-        guard getIsRequired() else {
-            return .success(true)
-        }
-
-        return validateContent()
-    }
-
-    public func handleResult(with validationResult: Result<Bool, Error>) -> Bool {
-        switch validationResult {
-        case .failure(let error):
-            guard let error = error as? FormFieldErrorType else {
-                return false
-            }
-
-            show(error: error)
-            return false
-        default:
-            clearError()
-            return true
-        }
-    }
-
-    public func validateAndHandle() -> Bool {
-        return handleResult(with: isValid())
-    }
-
-    public func show(error: FormFieldErrorType) {
-        isErrored = true
-        layer.borderColor = EmeraldTheme.redColor.cgColor
-    }
-
-    public func clearError() {
-        isErrored = false
-        layer.borderColor = EmeraldTheme.borderColor.cgColor
-    }
-
     public func getValue() -> String? {
         if text?.isEmpty ?? true {
             return ""
         }
         return text?.trimmingCharacters(in: .whitespacesAndNewlines)
-    }
-
-    public func setText(with value: String?) {
-        if let value = value, !value.isEmpty {
-            activateField()
-        } else {
-            deactivateField()
-        }
-        text = value
-    }
-
-    public func setCustomDelegate(with delegate: CustomEmeraldTextFieldDelegate) {
-        customTextFieldDelegate = delegate
     }
 
     func validateContent() -> Result<Bool, Error> {
@@ -472,5 +324,154 @@ public class EmeraldTextField: UITextFieldType, EmeraldTextFieldType, TextFormat
                 self.placeholderLabel.frame.origin = self.originCenter
             },
             completion: { _ in })
+    }
+}
+
+extension EmeraldTextField: EmeraldTextFieldType {
+    public func set(placeholder: String?) {
+        initialPlaceHolder = placeholder
+        placeholderLabel.text = placeholder
+        placeholderLabel.textColor = EmeraldTheme.placeholderColor
+    }
+
+    public func getPlaceholder() -> String? {
+        return initialPlaceHolder
+    }
+
+    public func set(hint: String?) {
+        self.hint = hint
+    }
+
+    public func getHint() -> String? {
+        return hint
+    }
+
+    public func setText(with value: String?) {
+        if let value = value, !value.isEmpty {
+            activateField()
+        } else {
+            deactivateField()
+        }
+        text = value
+    }
+
+    public func clearText() {
+        setText(with: nil)
+    }
+
+    public func set(id: String?) {
+        self.id = id
+    }
+
+    public func getId() -> String? {
+        return id
+    }
+
+    public func set(isRequired: Bool) {
+        self.isRequired = isRequired
+    }
+
+    public func getIsRequired() -> Bool {
+        return isRequired
+    }
+
+    public func isValid() -> Result<Bool, Error> {
+        guard getIsRequired() else {
+            return .success(true)
+        }
+
+        return validateContent()
+    }
+
+    public func handleResult(with validationResult: Result<Bool, Error>) -> Bool {
+        switch validationResult {
+        case .failure(let error):
+            guard let error = error as? FormFieldErrorType else {
+                return false
+            }
+
+            show(error: error)
+            return false
+        default:
+            clearError()
+            return true
+        }
+    }
+
+    public func show(error: FormFieldErrorType) {
+        isErrored = true
+        layer.borderColor = EmeraldTheme.redColor.cgColor
+    }
+
+    public func clearError() {
+        isErrored = false
+        layer.borderColor = EmeraldTheme.borderColor.cgColor
+    }
+
+    public func validateAndHandle() -> Bool {
+        return handleResult(with: isValid())
+    }
+
+    public func set(format: TextFormat) {
+        innerFormat = format
+        switch format {
+        case .currency:
+            set(maxLength: InnerConstants.maximumDoubleLength)
+            set(inputType: .decimalPad)
+        case .number:
+            set(inputType: .numberPad)
+        case .longDate:
+            set(maxLength: InnerConstants.maximumDateLength)
+            set(inputType: .numberPad)
+        case .shortDate:
+            set(maxLength: InnerConstants.maximumShortDateLength)
+            set(inputType: .numberPad)
+        default:
+            set(inputType: .asciiCapable)
+        }
+    }
+
+    public func getFormat() -> TextFormat {
+        return innerFormat
+    }
+
+    public func getUnformattedText() -> String? {
+        guard let textWithFormat = text else {
+            return nil
+        }
+
+        do {
+            return try remove(format: innerFormat, to: textWithFormat)
+        } catch {
+            return nil
+        }
+    }
+
+    public func set(inputType: UIKeyboardType) {
+        keyboardType = inputType
+    }
+
+    public func getInputType() -> UIKeyboardType? {
+        return keyboardType
+    }
+
+    public func set(maxLength: Int) {
+        if maxLength > 0 {
+            self.maxLength = maxLength
+        }
+    }
+
+    public func getMaxLength() -> Int {
+        return maxLength
+    }
+
+    public func isEnable(_ enable: Bool) {
+        alpha = enable == true ? 1 : 0.5
+        isEnabled = enable
+        deactivateField()
+    }
+
+    public func setCustomDelegate(with delegate: CustomEmeraldTextFieldDelegate) {
+        customTextFieldDelegate = delegate
     }
 }
