f48618222f7703766c99bca31fc44c9a1e33978b
David Polania <david.polania@condorlabs.io>
2022-08-17 14:44:17 -0500

    changue parameters type and create constant

--- EmeraldIOS/Components/SwiftUi/SwiftUICheckbox/EmeraldSwiftUICheckbox.swift
+++ EmeraldIOS/Components/SwiftUi/SwiftUICheckbox/EmeraldSwiftUICheckbox.swift
@@ -13,11 +13,13 @@ public struct EmeraldSwiftUICheckbox: View {
     var text: String
     
     private struct InnerConstant {
-        static let twenty = 12
-        static let ten = 10
-        static let thirty = 30
-        static let one = 1
-        static let three = 3
+        static let twenty: CGFloat = 12
+        static let ten: CGFloat = 10
+        static let thirty: CGFloat = 30
+        static let one: CGFloat = 1
+        static let three: CGFloat = 3
+        static let checkmarkImage = "checkmark.square.fill"
+        static let squareImage = "square"
     }
     
     public init(checked: Binding<Bool>, text: String) {
@@ -27,27 +29,29 @@ public struct EmeraldSwiftUICheckbox: View {
     
     public var body: some View {
         HStack {
-            Image(systemName: checked ? "checkmark.square.fill" : "square")
-                .resizable()
-                .scaledToFit()
-                .foregroundColor(checked ? EmeraldTheme.primaryColor.suColor :
-                                    EmeraldTheme.borderColor.suColor)
-                .frame(width: CGFloat(InnerConstant.twenty),
-                       height: CGFloat(InnerConstant.twenty))
+            Image(systemName: checked ?
+                  InnerConstant.checkmarkImage :
+                    InnerConstant.squareImage)
+            .resizable()
+            .scaledToFit()
+            .foregroundColor(checked ? EmeraldTheme.primaryColor.suColor :
+                                       EmeraldTheme.borderColor.suColor)
+            .frame(width: InnerConstant.twenty,
+                   height: InnerConstant.twenty)
             
             Spacer()
             
             Text(text)
                 .bodyBold()
         }
-        .padding(.vertical, CGFloat(InnerConstant.twenty))
-        .padding(.leading, CGFloat(InnerConstant.ten))
-        .padding(.trailing, CGFloat(InnerConstant.thirty))
+        .padding(.vertical, InnerConstant.twenty)
+        .padding(.leading, InnerConstant.ten)
+        .padding(.trailing, InnerConstant.thirty)
         .background(
-            RoundedRectangle(cornerRadius: CGFloat(InnerConstant.three))
+            RoundedRectangle(cornerRadius: InnerConstant.three)
                 .stroke(checked ? EmeraldTheme.primaryColor.suColor :
                             EmeraldTheme.borderColor.suColor,
-                        lineWidth: CGFloat(InnerConstant.one))
+                        lineWidth: InnerConstant.one)
         )
         .onTapGesture {
             withAnimation {
