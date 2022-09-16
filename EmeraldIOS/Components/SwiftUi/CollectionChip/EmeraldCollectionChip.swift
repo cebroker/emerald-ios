03a38e5fdf57c8232b5d084154c7de555fad98fa
David Polania <david.polania@condorlabs.io>
2022-09-16 16:58:09 -0500

    Resolve comment PR clean code

--- EmeraldIOS/Components/SwiftUi/CollectionChip/EmeraldCollectionChip.swift
+++ EmeraldIOS/Components/SwiftUi/CollectionChip/EmeraldCollectionChip.swift
@@ -24,7 +24,7 @@ public struct ChipsDataModel: Swift.Identifiable {
 public class ChipsViewLogic: ObservableObject {
     @Published var dataObject: [ChipsDataModel]
     
-    public init(){
+    public init() {
         self.dataObject = []
     }
     
